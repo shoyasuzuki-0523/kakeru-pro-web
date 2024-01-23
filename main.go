3e347957be3bbb72e1c46834896b1894c32a6351
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2024-01-24 03:07:09 +0900

    metaデータを動的に修正できるように

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Meta struct {
+	Title string
+}
+
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
 
 	r.GET("/", func(ctx *gin.Context) {
+		meta := Meta{Title: "Home"}
 		ctx.HTML(200, "index.html", gin.H{
+			"meta": meta,
 			"data": "This is Home.",
 		})
 	})
 
 	r.GET("/about", func(ctx *gin.Context) {
+		meta := Meta{Title: "About"}
 		ctx.HTML(200, "index.html", gin.H{
+			"meta": meta,
 			"data": "This is About.",
 		})
 	})
